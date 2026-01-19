fb9c126198ab2f9cdf17be2aa257cb2a502f121a
AntWills <antoniowillissilvasantos@gmail.com>
2026-01-19 09:24:26 -0300

    initial commit

---
+++ src/main/java/com/comp/br/CcompApplication.java
@@ -0,0 +1,21 @@
+package com.comp.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CcompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcompApplication.class, args);
+	}
+
+    @GetMapping
+    public String hello(){
+        return "Ola mundo";
+    }
+}
