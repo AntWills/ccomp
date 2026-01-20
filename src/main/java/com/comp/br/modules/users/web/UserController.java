f2f9e08af5b776546d49b9f23c57c54673f0ddb5
AntWills <antoniowillissilvasantos@gmail.com>
2026-01-20 08:34:05 -0300

    config: Configurar path da rota.

--- src/main/java/com/comp/br/modules/users/web/UserController.java
+++ src/main/java/com/comp/br/modules/users/web/UserController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("api/users")
+@RequestMapping("users")
 public class UserController {
     private List<UserModel> users = new ArrayList<>();
 
