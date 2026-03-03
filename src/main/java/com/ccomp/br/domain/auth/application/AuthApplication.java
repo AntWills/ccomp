f3a8d75d4b4ee21e48b86ba8d470fdd2ef5f5ad6
AntWills <antoniowillissilvasantos@gmail.com>
2026-03-03 17:33:20 -0300

    feat[Config]: Configurar async com virtual threads.

--- src/main/java/com/ccomp/br/domain/auth/application/AuthApplication.java
+++ src/main/java/com/ccomp/br/domain/auth/application/AuthApplication.java
@@ -1,16 +1,22 @@
 package com.ccomp.br.domain.auth.application;
 
-import com.ccomp.br.domain.auth.dto.AuthResponse;
+import com.ccomp.br.config.AsyncConfig;
+import com.ccomp.br.domain.auth.dto.RefreshTokenRequest;
+import com.ccomp.br.domain.auth.dto.RefreshTokenResponse;
+import com.ccomp.br.domain.auth.dto.SignInResponse;
 import com.ccomp.br.domain.auth.dto.LoginRequestDTO;
 import com.ccomp.br.domain.users.management.UserManagement;
 import com.ccomp.br.shared.dto.RegisterUserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AuthApplication {
     private final UserManagement userManagement;
-
+//    private final
     private final JwtService jwtService;
 
     @Autowired
@@ -19,16 +25,25 @@ public class AuthApplication {
         this.jwtService = jwtService;
     }
 
-    public AuthResponse register(RegisterUserDTO dto) {
-        var userDto = userManagement.register(dto);
-
-        return new AuthResponse(jwtService.getAccessToken(userDto.id()),
-                jwtService.getExpirationInSeconds());
+    public void signUp(RegisterUserDTO dto) {
+        userManagement.register(dto);
     }
 
-    public AuthResponse login(LoginRequestDTO dto) {
-        var userDTO = userManagement.validateCredentials(dto.email(), dto.password());
+    public SignInResponse signIn(LoginRequestDTO dto) {
+        var userDto = userManagement.validateCredentials(dto.email(), dto.password());
 
-        return new AuthResponse(jwtService.getAccessToken(userDTO.id()), jwtService.getExpirationInSeconds());
+        return new SignInResponse(
+                jwtService.getAccessToken(userDto.id()),
+                jwtService.getRefreshToken(userDto.id()).getToken());
+    }
+
+    public Optional<RefreshTokenResponse> refresh(RefreshTokenRequest request){
+        return jwtService.validRefreshToken(request)
+                .map(RefreshTokenResponse::new);
+    }
+
+    @Async(AsyncConfig.VIRTUAL_TASK_EXECUTOR)
+    public void logoutI(RefreshTokenRequest request){
+        jwtService.deleteRefreshToken(request);
     }
 }
