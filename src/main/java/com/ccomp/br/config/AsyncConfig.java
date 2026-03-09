994e0216448d2c672530af75e0e123c6c914290e
AntWills <antoniowillissilvasantos@gmail.com>
2026-03-09 09:04:35 -0300

    refactor: Agora bastar usar @Async em um método para ele  ser um asyncrono, sem mais parametros.

--- src/main/java/com/ccomp/br/config/AsyncConfig.java
+++ src/main/java/com/ccomp/br/config/AsyncConfig.java
@@ -12,13 +12,4 @@ import java.util.concurrent.Executors;
 @Configuration
 @EnableAsync
 public class AsyncConfig {
-
-    public static final String VIRTUAL_TASK_EXECUTOR = "virtualTaskExecutor";
-
-    @Bean(name = VIRTUAL_TASK_EXECUTOR)
-    public Executor taskExecutor() {
-        return Executors.newThreadPerTaskExecutor(
-                Thread.ofVirtual().name("vthread-", 0).factory()
-        );
-    }
 }
