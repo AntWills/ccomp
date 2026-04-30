97526ccebde1e21694ee616c5536fb8eb837829b
AntWills <antoniowillissilvasantos@gmail.com>
2026-04-30 09:15:37 -0300

    feat(news): Criar controllers para interação com o domain de serviços.
    - Criar o arquivo `NewsController` com as rotas de create, getById e update.
    - Adicionar ao `NewsApplication` o método getById.
    - Corrigir a comparação do usuario e autor em `NewsApplication.update`.
    - Alterar perfil dev para atualizar o schema do banco, evitando apagar e recriar ao subir um novo `application-dev.properties`.

--- src/main/java/com/ccomp/br/domain/news/application/NewsApplication.java
+++ src/main/java/com/ccomp/br/domain/news/application/NewsApplication.java
@@ -6,12 +6,14 @@ import com.ccomp.br.domain.news.persistence.ContentBlock;
 import com.ccomp.br.domain.news.persistence.News;
 import com.ccomp.br.domain.news.persistence.NewsRepository;
 import com.ccomp.br.domain.news.util.NewsMapper;
+import com.ccomp.br.shared.exceptions.AccessDeniedException;
 import com.ccomp.br.shared.exceptions.ResourceNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -24,6 +26,7 @@ public class NewsApplication {
         this.newsMapper = newsMapper;
     }
 
+    @Transactional
     public News create(UUID authorId) {
         List<ContentBlock> blocks = List.of(
                 new ContentBlock(1L, ContentBlockType.HEADING.toString(), "News headline", null, null, null, null),
@@ -40,14 +43,19 @@ public class NewsApplication {
     }
 
     @Transactional
-    public News update(NewsUpdateDto dto) {
+    public News update(NewsUpdateDto dto, UUID userId) {
         News entity = newsRepository.findById(dto.id())
                 .orElseThrow(() -> new ResourceNotFoundException("Notícia não encontrada."));
 
+        if(!userId.equals(entity.getAuthorId())) throw new AccessDeniedException("O usuário não tem acesso a este recurso.");
         newsMapper.updateEntityFromDto(dto, entity);
 
         newsRepository.save(entity);
 
         return entity;
+    }
+
+    public Optional<News> getById(Long id) {
+        return newsRepository.findById(id);
     }
 }
---
+++ src/main/java/com/ccomp/br/domain/news/web/NewsController.java
@@ -0,0 +1,47 @@
+package com.ccomp.br.domain.news.web;
+
+import com.ccomp.br.domain.news.application.NewsApplication;
+import com.ccomp.br.domain.news.dto.NewsUpdateDto;
+import com.ccomp.br.domain.news.persistence.News;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/news")
+public class NewsController {
+    private final NewsApplication newsApplication;
+
+    public NewsController(NewsApplication newsApplication) {
+        this.newsApplication = newsApplication;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> create(@AuthenticationPrincipal Jwt jwt){
+        News entity = newsApplication.create(UUID.fromString(jwt.getSubject()));
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(entity);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getById(@PathVariable Long id, @AuthenticationPrincipal Jwt jwt) {
+        var response = newsApplication.getById(id);
+
+        if(response.isPresent()) {
+            return ResponseEntity.status(HttpStatus.OK).body(response.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PatchMapping
+    public ResponseEntity<?> update(@Valid @RequestBody NewsUpdateDto dto, @AuthenticationPrincipal Jwt jwt) {
+        News entity = newsApplication.update(dto, UUID.fromString(jwt.getSubject()));
+
+        return ResponseEntity.status(HttpStatus.OK).body(entity);
+    }
+}
