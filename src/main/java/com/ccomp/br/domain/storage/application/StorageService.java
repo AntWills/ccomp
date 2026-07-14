2396a81ff33871088310ca7b33b8a4e7848c40ae
AntWills <antoniowillissilvasantos@gmail.com>
2026-07-14 10:11:15 -0300

    feat: Tornando o serviço de Storage mais resistente a erros.

--- src/main/java/com/ccomp/br/domain/storage/application/StorageService.java
+++ src/main/java/com/ccomp/br/domain/storage/application/StorageService.java
@@ -1,12 +1,14 @@
 package com.ccomp.br.domain.storage.application;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.ResponseBytes;
+import software.amazon.awssdk.core.exception.SdkException;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
@@ -17,6 +19,7 @@ import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class StorageService {
 
     private final S3Client s3Client;
@@ -27,30 +30,47 @@ public class StorageService {
     @Value("${storage.endpoint}")
     private String endpoint;
 
-    public String upload(MultipartFile file, String fileName) throws IOException {
-        s3Client.putObject(
-                PutObjectRequest.builder()
-                        .bucket(bucket)
-                        .key(fileName)
-                        .contentType(file.getContentType())
-                        .build(),
-                RequestBody.fromBytes(file.getBytes()));
+    public String upload(MultipartFile file, String fileName) {
+        try {
+            s3Client.putObject(
+                    PutObjectRequest.builder()
+                            .bucket(bucket)
+                            .key(fileName)
+                            .contentType(file.getContentType())
+                            .build(),
+                    RequestBody.fromBytes(file.getBytes()));
 
-        return endpoint + "/" + bucket + "/" + fileName;
+            return endpoint + "/" + bucket + "/" + fileName;
+        } catch (IOException | SdkException e) {
+            log.error("Não foi possível enviar o arquivo {} para o bucket {}!", fileName, bucket);
+            log.error(e.getMessage());
+            return null;
+        }
     }
 
     public Resource findByFileName(String fileName) {
-        ResponseBytes<GetObjectResponse> response = s3Client.getObjectAsBytes(request ->
-                request.bucket(bucket).key(fileName));
+        try {
+            ResponseBytes<GetObjectResponse> response = s3Client.getObjectAsBytes(request ->
+                    request.bucket(bucket).key(fileName));
 
-        return new ByteArrayResource(response.asByteArray());
+            return new ByteArrayResource(response.asByteArray());
+        } catch (SdkException e) {
+            log.error("Não foi possível encontrar o arquivo {} no bucket {}!", fileName, bucket);
+            log.error(e.getMessage());
+            return null;
+        }
     }
 
     public void delete(String fileName) {
-        s3Client.deleteObject(
-                DeleteObjectRequest.builder()
-                        .bucket(bucket)
-                        .key(fileName)
-                        .build());
+        try {
+            s3Client.deleteObject(
+                    DeleteObjectRequest.builder()
+                            .bucket(bucket)
+                            .key(fileName)
+                            .build());
+        } catch (SdkException e) {
+            log.error("Não foi possível deletar o arquivo {} do bucket {}!", fileName, bucket);
+            log.error(e.getMessage());
+        }
     }
 }
