c58e06915dfc5223af4a0acebee8d1e3b6386c4f
AntWills <antoniowillissilvasantos@gmail.com>
2026-07-16 09:30:22 -0300

    feat(SecurityConfig): Aperfeiçoar segurança ao acessar o swagger. Agora, ao tentar acessar, ira aparecer um pop-up pedindo usuario e senha.

--- src/main/java/com/ccomp/br/config/SecurityConfig.java
+++ src/main/java/com/ccomp/br/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.ccomp.br.config;
 
 import com.ccomp.br.domain.users.security.CustomAuthEntryPoint;
+import com.ccomp.br.domain.users.security.UserDetailsImpl;
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -50,15 +52,6 @@ public class SecurityConfig {
     @Value("${jwt.public.key}")
     private RSAPublicKey publicKey;
 
-//    @Value("${swagger.security.enabled:true}")
-//    private boolean swaggerSecurityEnabled;
-//
-//    @Value("${swagger.username}")
-//    private String swaggerUsername;
-//
-//    @Value("${swagger.password}")
-//    private String swaggerPassword;
-
     private static final String[] PUBLIC_AUTH_ROUTES = {
             "/api/auth/**"
     };
@@ -81,43 +74,26 @@ public class SecurityConfig {
             "/v3/api-docs"
     };
 
-//    @Bean
-//    @Order(1)
-//    public SecurityFilterChain swaggerFilterChain(HttpSecurity http) throws Exception {
-//        http.securityMatcher(SWAGGER_ROUTES)
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-//
-//        if (swaggerSecurityEnabled) {
-//            // Cria um AuthenticationManager isolado, só para o Swagger.
-//            // NÃO usa o AuthenticationManagerBuilder compartilhado do HttpSecurity,
-//            // evitando conflito com o AuthenticationManager global usado no login.
-//            DaoAuthenticationProvider swaggerProvider = new DaoAuthenticationProvider(swaggerUserDetailsService());
-//            swaggerProvider.setPasswordEncoder(passwordEncoder());
-//
-//            AuthenticationManager swaggerAuthManager = new ProviderManager(swaggerProvider);
-//
-//            http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
-//                    .authenticationManager(swaggerAuthManager) // <- isolado, não é o bean global
-//                    .httpBasic(basic -> {});
-//        } else {
-//            http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
-//        }
-//
-//        return http.build();
-//    }
+    @Bean
+    @Order(1)
+    public SecurityFilterChain swaggerFilterChain(HttpSecurity http, UserDetailsService userDetailsService) throws Exception {
+        DaoAuthenticationProvider swaggerProvider = new DaoAuthenticationProvider(userDetailsService);
+        swaggerProvider.setPasswordEncoder(this.passwordEncoder());
+        AuthenticationManager swaggerAuthManager = new ProviderManager(swaggerProvider);
 
-//    @Bean
-//    public UserDetailsService swaggerUserDetailsService() {
-//        var user = User.withUsername(swaggerUsername)
-//                .password(passwordEncoder().encode(swaggerPassword))
-//                .roles("SWAGGER")
-//                .build();
-//        return new InMemoryUserDetailsManager(user);
-//    }
+        http.securityMatcher(SWAGGER_ROUTES)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .authenticationManager(swaggerAuthManager)
+                .httpBasic(Customizer.withDefaults());
+
+
+        return http.build();
+    }
 
     @Bean
-//    @Order(2)
+    @Order(2)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
@@ -130,7 +106,7 @@ public class SecurityConfig {
                             // News
                             authorize.requestMatchers(HttpMethod.GET, PUBLIC_NEWS_ROUTES).permitAll();
                             // Documentação
-                            authorize.requestMatchers(SWAGGER_ROUTES).permitAll();
+//                            authorize.requestMatchers(SWAGGER_ROUTES).permitAll();
 
                             authorize.anyRequest().authenticated();
                         }
