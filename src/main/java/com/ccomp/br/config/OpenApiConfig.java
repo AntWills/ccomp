3c963b306234e9462fec7bbd5b964bd0c0b89859
AntWills <antoniowillissilvasantos@gmail.com>
2026-07-29 17:37:22 -0300

    feat(gateway): configurar roteamento com nginx e suporte a proxy reverso no backend
    - Adiciona regras de proxy reverso no Nginx enviando o trafego da raiz '/' para o ccomp-backend.
    - Redireciona a rota '/' diretamente para a interface do Swagger UI ('/swagger-ui/index.html').
    - Configura o resolver DNS interno do Docker (127.0.0.11) e repassa os cabecalhos 'X-Forwarded-*' no Nginx.
    - Oculta a porta 8080 direta do 'ccomp-backend' no docker-compose.dev.yml, centralizando os acessos pelo gateway.
    - Configura 'server.forward-headers-strategy=FRAMEWORK' no application.properties para processar cabecalhos de proxy.
    - Adiciona URL relativa ('/') na configuracao do OpenApiConfig para funcionamento correto do Swagger via gateway.

--- src/main/java/com/ccomp/br/config/OpenApiConfig.java
+++ src/main/java/com/ccomp/br/config/OpenApiConfig.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -37,6 +38,7 @@ public class OpenApiConfig {
         final String securitySchemeName = "bearerAuth";
 
         return new OpenAPI()
+                .servers(List.of(new Server().url("/")))
                 .info(new Info()
                         .title("CCOMP API")
                         .version(apiVersion)
