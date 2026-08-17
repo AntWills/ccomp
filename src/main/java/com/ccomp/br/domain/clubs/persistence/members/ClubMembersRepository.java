fea708c952842422eea3f6010e0cbd6dafc40a1f
AntWills <antoniowillissilvasantos@gmail.com>
2026-08-17 18:15:34 -0300

    feat(clubs): Criar entidate de representa os membros inscritos de um club.

---
+++ src/main/java/com/ccomp/br/domain/clubs/persistence/members/ClubMembersRepository.java
@@ -0,0 +1,6 @@
+package com.ccomp.br.domain.clubs.persistence.members;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClubMembersRepository extends JpaRepository<ClubMembers, Long> {
+}
