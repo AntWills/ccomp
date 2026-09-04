054fdd70ffa5176d2e5b4b7a20e3cc21c5cb48cd
AntWills <antoniowillissilvasantos@gmail.com>
2026-09-04 07:52:02 -0300

    fix: Ajustar importação de NonNUll.

--- src/main/java/com/ccomp/br/domain/security/UserDetailsImpl.java
+++ src/main/java/com/ccomp/br/domain/security/UserDetailsImpl.java
@@ -3,7 +3,7 @@ package com.ccomp.br.domain.security;
 import com.ccomp.br.domain.users.enums.EnumRoles;
 import com.ccomp.br.domain.users.enums.EnumUserStatusAccount;
 import com.ccomp.br.shared.dto.UserDTO;
-import lombok.NonNull;
+import org.jspecify.annotations.NonNull;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
